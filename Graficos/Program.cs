8c874afd877e8bb5130d3af6f502ce43d3091153
UFG-01 <UFG-01@LDI-14.ufg.local>
2023-09-25 11:10:32 -0600

    25/09/2023: continuación grafico

--- Graficos/Program.cs
+++ Graficos/Program.cs
@@ -1,8 +1,11 @@
+using Graficos.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<VentasContext>(opcions=>opcions.UseSqlServer("cadenaSQL"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
